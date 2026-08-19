f7a60651402d1104083c02c7d4ca127070b8317e
BuSheey <6915170+busheezy@users.noreply.github.com>
2026-08-19 06:22:15 -0500

    init commit

---
+++ prisma.config.ts
@@ -0,0 +1,12 @@
+import "dotenv-expand/config";
+import { defineConfig } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: process.env["DATABASE_URL"],
+  },
+});
